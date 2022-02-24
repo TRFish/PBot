0a410d1bf9e9d32ebdeda268ed278131191e743c
TheRussianFish <sasha.brustovskiy2015@gmail.com>
2022-02-24 15:37:51 +0900

    Первый мусор

---
+++ b_back.py
@@ -0,0 +1,14 @@
+###### Мусор! Не трогай! #####
+
+# Модуль дополнительных функций
+# Не используется
+
+
+
+#def achievement(name):
+#	if name == 0:
+#		Achievement_name = name + '_N'
+#		Achievement_value = name + '_V'
+#		print(Achievement_add, Achievement_name)
+#		Achievement_value = 1
+# ^ Не работает ^
