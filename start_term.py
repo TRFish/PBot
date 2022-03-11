e0e27d37d9ec490da8daa5916816a415d4626ed1
TRFish <sasha.brustovskiy2015@gmail.com>
2022-03-11 16:12:23 +0900

    Минимум строк - минимум вопросов.

--- start_term.py
+++ start_term.py
@@ -1,9 +1,6 @@
 #!/bin/python
-
 from backend import *
-
-# Еще один безполезный комент объясняющий смысл и так понятного куска кода
 command = 'start'
 while command != 'exit':
 	input_analyzer(command)
-	command = input('\n>> ')
+	command = input('>> ')
