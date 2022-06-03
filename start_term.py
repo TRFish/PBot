7ed7fd51e36a3ac10ab7f0c92b1e29efb5785a3f
TRFish <sasha.brustovskiy2015@gmail.com>
2022-06-04 04:15:08 +0900

    пам-пам

--- start_term.py
+++ start_term.py
@@ -8,9 +8,6 @@ from image import aprint as image # почему я только СЕЙЧАС у
 class Bot():
 	def __init__(self, botType='Terminal'):
 		self.bot_type  = botType
-		self.t_start   = f'Привет, меня зовут {config.Name}, бот на все случаи жизни(ну или на те что предусмотрены кодом)\nЧтобы вывести список команд напиши help'
-		self.t_help    = 'Список команд пока не доступен'
-		self.t_news    = 'Новое в Боте:\n  Start использует f-строку (а зачем? (а чтобы в переменной t_start была одна строка, а не две(а оно мешало?)))'
 	
 	def input_analyzer(self, command):
 		command = command.lower()
@@ -44,25 +41,25 @@ class Bot():
 			self.rando()
 	
 	def dialog(self):
-		print('Режим разговора. Тут ты можешь действительно поболтать о чем угодно. Чтобы выйти напиши "exit"')
+		print(text.dialog_welcome)
 		self.text = input()
 		while self.text != 'exit':
 			#Достижение(Скам мамонта №1)
 			if config.A_Mamont_V == 0:
-				print('Хаха\nТы думал, я сюда ИИ запихал?')
-				print('Вы получили достижение: Мамонт заскамлен')
+				print(text.dialog_r1)
+				print(text.ach_received, text.ach_mamont)
 				config.A_Mamont_V = 1
-				print('АХАХАХАХАХА')
-			self.text = input('\nДля выхода напиши "exit":\n')
+				print(text.dialog_r2)
+			self.text = input(text.tip_exit)
 	
 	def new(self):
 		image('dino')
-		print('Я сделяль новые функции, а это динозавр их сломал')
+		print(text.new_rep1)
 		time.sleep(0.1)
-		print('Честно\n')
+		print(text.new_rep2)
 		time.sleep(4)
 		image('snail')
-		print('Но я уже делаю новые со скоростью угашенной улитки!')
+		print(text.new_rep3)
 	
 	def echo_mode(self):
 		self.text = None
@@ -70,38 +67,46 @@ class Bot():
 			while self.text != 'Please!':
 				while self.text != 'please':
 					while self.text != 'stop':
-						self.text = input('Введите любой текст(Чтобы выйти напишите stop):')
+						self.text = input(f'{text.tip_text} {text.echomode_ex1}')
 						print(self.text)
-					self.text = input('Введите любой текст(А волшебное слово):')
+					self.text = input(f'{text.tip_text} {text.echomode_ex2}')
 					print(self.text)
-				self.text = input('Введите любой текст(Не расслышал):')
+				self.text = input(f'{text.tip_text} {text.echomode_ex3}')
 				print(self.text)
-			self.text = input('Введите любой текст(А теперь всё вместе):')
+			self.text = input(f'{text.tip_text} {text.echomode_ex4}')
 			print(self.text)
-		print('Ну ладно, ладно. Не бугурти.')
+		print(text.echomode_exit)
 	
 	def rando(self):
-		self.minimal = int(input('Минимальное число:'))
-		self.maximal = int(input('Максимальное число:'))
+		self.minimal = int(input(text.rando_rep1))
+		self.maximal = int(input(text.rando_rep2))
 		self.output  = random.randint(self.minimal, self.maximal)
 		print(self.output)
 	
 	def news(self):
-		print(self.t_news)
+		print(text.news)
 	
 	def help(self):
-		print(self.t_help)
+		print(text.help)
 	
 	def start(self):
-		print(self.t_start)
+		print(text.start)
 
 if __name__ == '__main__':
+	# Lang chooser
+	lng = input('Choose language:\n 1 - Russian\n 2 - English\n>>')
+	if lng == '1' or lng == '':
+		import lang as text
+	else:
+		print('Error')
+		exit()
+	# Main
 	bot = Bot()
 	command = 'start'
 	while command != 'exit':
 		bot.input_analyzer(command)
 		command = input('>> ')
-	print('Bye!')
+	print(text.exit)
 else:
-	print(f'Использование бота ({config.Name}) как модуль другого проекта не предусмотренно!')
+	print(f'Использование бота ({config.Name}) как модуль другого проекта не предусмотренно!(ну и для чего я эту защиту сделал...)')
 	exit()
